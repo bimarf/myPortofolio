c0f6db8e3febf6573db6d8c0f291227ec5bb3247
bimarf <bimarenggaxvi@gmail.com>
2023-07-06 15:56:48 +0700

    web portofolio

---
+++ tailwind.config.js
@@ -0,0 +1,24 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["index.html"],
+  theme: {
+    container: {
+      center: true,
+      padding: '16px',
+    },
+    extend: {
+      colors: {
+        text: "#0e1025",
+        background: "#fbfbfe",
+        primarybutton: "#af93d7",
+        secondarybutton: "#ffffff",
+        accent: "#d193d7",
+        secbg: "#2c5d87",
+      },
+      screens: {
+        '2xl': '1320px',
+      },
+    },
+  },
+  plugins: [],
+};
